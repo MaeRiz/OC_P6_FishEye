a5d0bd715bffc42bc3d83ab6b46edead2510d161
Mahery Gonin <mahery.gonin@gmail.com>
2022-11-01 15:24:01 +0100

    add keyboard controls for homepage

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,33 +1,40 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
+async function getPhotographers() {
+    // Penser à remplacer par les données récupérées dans le json
 
-        const response = await fetch('../../data/photographers.json');
-        const data = await response.json();
+    const response = await fetch('../../data/photographers.json');
+    const data = await response.json();
 
-        return ({ photographers: data.photographers});
+    return ({ photographers: data.photographers});
+}
+
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+
+        const link = document.createElement( 'a' );
+        link.setAttribute("href", "/photographer.html?id=" + photographer.id);
+        link.setAttribute("tabindex", "-1");
+
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        link.appendChild(userCardDOM);
+        photographersSection.appendChild(link);
+    });
+};
+
+    // Keyboard controls for open photographer page in focus with enter or space
+document.addEventListener('keydown', event => {
+    if (event.code === "Space" || event.key === "Enter") {
+        event.target.click();
+        event.preventDefault();
     }
+})
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
 
-        photographers.forEach((photographer) => {
-
-            const link = document.createElement( 'a' );
-            link.setAttribute("href", "/photographer.html?id=" + photographer.id);
-            link.setAttribute("tabindex", "-1");
-
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            link.appendChild(userCardDOM);
-            photographersSection.appendChild(link);
-        });
-    };
-
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+init();
