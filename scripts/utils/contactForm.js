e6f2435b30ca38f923b53f6c39e213154a921252
Mahery Gonin <mahery.gonin@gmail.com>
2022-11-06 16:22:05 +0100

    Modal: keep focus, change submit style

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,5 +1,36 @@
 const formDOM = document.querySelector("form");
 
+// Keep focus in modal
+const  focusableElements =
+    'button, input, textarea, [tabindex]:not([tabindex="-1"])';
+const modal = document.querySelector('#contact_modal'); // select the modal by it's id
+
+const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+const focusableContent = modal.querySelectorAll(focusableElements);
+const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+
+document.addEventListener('keydown', function(e) {
+  let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+  if (!isTabPressed || modal.style.display === "none") {
+    return;
+  }
+
+  if (e.shiftKey) { // if shift key pressed for shift + tab combination
+    if (document.activeElement === firstFocusableElement) {
+      lastFocusableElement.focus(); // add focus for the last focusable element
+      e.preventDefault();
+    }
+  } else { // if tab key is pressed
+    if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+      firstFocusableElement.focus(); // add focus for the first focusable element
+      e.preventDefault();
+    }
+  }
+});
+
+
 function displayModal() {
     const modal = document.getElementById("contact_modal");
     const mainDOM = document.querySelector("#main-photographer-page");
@@ -9,6 +40,7 @@ function displayModal() {
     mainDOM.setAttribute("aria-hidden", true);
     modal.setAttribute("aria-hidden", false);
     body.classList.add("no-scroll");
+    firstFocusableElement.focus();
 }
 
 function closeModal() {
